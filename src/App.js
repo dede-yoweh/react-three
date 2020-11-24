a82d65a263bb2fee03a52486a44940037ff2ec53
awurama <mimidede222@gmail.com>
2020-11-24 08:48:31 -0800

    react-trois

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <h1>come to us with your legal issues</h1>
+    </div>
+  );
+}
+
+export default App;
